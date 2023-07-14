2bf95d5769958502b8ac385a50fb754dfc00bab8
EmranSWE <mdemran.swe@gmail.com>
2023-07-14 23:40:23 +0600

    Checking the landing page using dummy data

--- src/page/Books.tsx
+++ src/page/Books.tsx
@@ -1,5 +1,27 @@
 import React from "react";
 
-export default function Books() {
-  return <div>Books</div>;
-}
+type BooksProps = {
+  title: string;
+  author: string;
+  genre: string;
+  publicationDate: string;
+};
+
+const Books: React.FC<BooksProps> = ({
+  title,
+  author,
+  genre,
+  publicationDate,
+}) => {
+  console.log(title);
+  return (
+    <div className="bg-white shadow-md rounded-lg p-4">
+      <h2 className="text-xl font-bold mb-2">{title}</h2>
+      <p className="text-gray-600">Author: {author}</p>
+      <p className="text-gray-600">Genre: {genre}</p>
+      <p className="text-gray-600">Publication Date: {publicationDate}</p>
+    </div>
+  );
+};
+
+export default Books;
